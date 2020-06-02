70ca4b3fdd023109078abf188127c7cc869d87bd
Zolushka <levshinkirill00@gmail.com>
2020-06-02 11:58:41 +0300

    minor changes in app.py and deleted .idea from the repository

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-import flask
+from flask import Flask, render_template, request
 import requests
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
+
+def getCurrentUrl(searchText):
+    return "http://api.giphy.com/v1/gifs/search?q=" + searchText + "&api_key=BJ9cysqXzWePISsT67nRxfP2Ku0dmPQg&limit=20"
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if flask.request.method == 'GET':
-        gifs = requests.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=BJ9cysqXzWePISsT67nRxfP2Ku0dmPQg&limit=20").json()
-        return flask.render_template("app.html", gifs=gifs['data'])
+    if request.method == 'GET':
+        gifs = requests.get(getCurrentUrl("cats")).json()
+        return render_template("app.html", gifs=gifs['data'])
     else:
-        gifs = requests.get("http://api.giphy.com/v1/gifs/search?q="+flask.request.form.get('searchText')+"&api_key=BJ9cysqXzWePISsT67nRxfP2Ku0dmPQg&limit=20").json()
-        return flask.render_template("app.html", gifs=gifs['data'])
+        gifs = requests.get(getCurrentUrl(request.form.get('searchText'))).json()
+        return render_template("app.html", gifs=gifs['data'])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
